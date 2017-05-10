72e20280a2d61537b52b9d2fc06aff7d8948529c
ChenJd <shaun.chan@hotmail.com>
2017-05-10 20:44:41 +0800

    add

---
+++ Assets/Script/TestWithGetComponent.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestWithGetComponent : MonoBehaviour {
+
+    private int testCount = 1000000;//定义测试的次数
+
+    void Start () {
+
+        Transform transformTest;
+
+        using (new YiWatch("GetComponent<>", testCount))
+        {
+            for(int i = 0; i < testCount; i++)
+            {
+                GetComponent<TestComp>();
+            }
+        }
+
+        using (new YiWatch("GetComponent(typeof(T))", testCount))
+        {
+            for(int i = 0; i < testCount; i++)
+            {
+                GetComponent(typeof(TestComp));
+            }
+        }
+
+        using (new YiWatch("GetComponent(string)", testCount))
+        {
+            for(int i = 0; i < testCount; i++)
+            {
+                GetComponent("TestComp");
+            }
+        }
+
+        using (new YiWatch("transform", testCount))
+        {
+            for(int i = 0; i < testCount; i++)
+            {
+                transformTest = this.transform;
+            }
+        }
+    }
+
+    void Update()
+    {
+        //for(int i = 0; i < testCount; i++)
+        //{
+        //    GetComponent<TestComp>();
+        //}
+    }
+	
+}
---
+++ Assets/Script/YiWatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+/// <summary>
+/// 简易的计时类
+/// </summary>
+public class YiWatch : IDisposable
+{
+    #region 字段
+
+    private string testName;
+    private int testCount;
+    private Stopwatch watch;
+
+    #endregion
+
+
+    #region 构造函数
+
+    public YiWatch(string name, int count)
+    {
+        this.testName = name;
+
+        this.testCount = count > 0 ? count : 1;
+
+        this.watch = Stopwatch.StartNew();
+    }
+
+    #endregion
+
+
+    #region 方法
+    public void Dispose()
+    {
+        this.watch.Stop();
+
+        float totalTime = this.watch.ElapsedMilliseconds;
+
+        UnityEngine.Debug.Log(string.Format("测试名称：{0}   总耗时：{1}   单次耗时：{2}    测试数量：{3}",
+            this.testName, totalTime, totalTime / this.testCount, this.testCount));
+    }
+
+    #endregion
+
+}
